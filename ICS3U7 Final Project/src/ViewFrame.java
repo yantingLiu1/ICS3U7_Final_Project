cae83a72486aab9326a6760a72ccf46ff0a1177f
RoshanYan <78375985+RoshanYan@users.noreply.github.com>
2022-01-20 10:11:12 -0500

    Add files via upload

--- ICS3U7 Final Project/src/ViewFrame.java
+++ ICS3U7 Final Project/src/ViewFrame.java
@@ -5,14 +5,14 @@ import java.awt.event.ActionListener;
 public class ViewFrame {
     public static void main(String[] args){
         viewf frame = new viewf();
-        frame.setSize(900,650);
+        frame.setSize(900,500);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setTitle("View");
         frame.setVisible(true);
     }
 }
-class viewf extends JFrame implements ActionListener{
+class viewf extends JFrame implements   ActionListener{
     viewf(){
         setLayout(null);
         String firstname = "a";
@@ -21,9 +21,15 @@ class viewf extends JFrame implements ActionListener{
         String address = "d";
         String city = "e";
         String province = "f";
-        String notes= "sadsafsadasfasasdgda\nsdgasfgsdagssfaerhsfs";
+        String notesin = "sadsafsadasfasasdgda\nsdgasfgsdagssfaerhsfs";
+        String[] notes = notesin.split("\n");
 
-        add(new CustomLabel(firstname,30, 40, 150, 30));
+        add(new CustomLabel(firstname+" "+lastname+" - "+phone,30, 70,500, 30));
+        add(new CustomLabel(address+", "+city+", "+province,30, 90,500, 30));
+        add(new CustomLabel("notes:",30,130,100,30));
+        for(int i = 0; i < notes.length;i++){
+            add(new CustomLabel(notes[i],30,150+i*15,800,30));
+        }
     }
 
     @Override
